6007dae959746e459572ec31be075968b3c5a393
mk812y <mk812y@gmail.com>
2023-09-07 22:02:54 +0200

    для себя сохраним тут

--- Basic Example of SwiftUI Menu/ContentView.swift
+++ Basic Example of SwiftUI Menu/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    var pickerOptions: [String] = ["Option 1", "Option 2", "Option 3"]
+    @State var selectionOption = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Menu {
+            Button(action: { print("Button 1")}
+            ) {
+                Text("Button one")
+            }
+            Section("Header section two") {
+                Button(role: .destructive, action: { print("Button 2 destructive")}
+                ) {
+                    Label("Delete", systemImage: "minus.circle")
+                }
+                Button(action: { print("Button 3 book")}
+                ) {
+                    Label("Book", systemImage: "book")
+                }
+                Menu {
+                    Button(action: {
+                        print("Submenu Button 3 book")
+                        
+                    }, label: {
+                        Label("Submenu Book", systemImage: "figure.wave")
+                        
+                    })
+                }label: {
+                    Label("Submen", systemImage: "folder")
+                }
+            }
+            Section("Header selection menu") {
+                Button(action: { print("Button 4")}
+                ) {
+                    Text("Button four")
+                }
+                Picker("Option", selection: $selectionOption) {
+                    ForEach(pickerOptions, id: \.self) { option in
+                        Text(option)
+                    }
+                }
+                .onChange(of: selectionOption) {
+                    print(selectionOption)
+                }
+            }
+        } label: {
+            Text("Menu")
         }
-        .padding()
     }
 }
 
