0262ad8d2f08ab716e35b00a8658ea4cb333e268
mk812y <mk812y@gmail.com>
2023-09-07 21:28:27 +0200

    Initial Commit

---
+++ Basic Example of SwiftUI Menu/Basic_Example_of_SwiftUI_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_Example_of_SwiftUI_MenuApp.swift
+//  Basic Example of SwiftUI Menu
+//
+//  Created by Михаил Куприянов on 7.9.23..
+//
+
+import SwiftUI
+
+@main
+struct Basic_Example_of_SwiftUI_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
